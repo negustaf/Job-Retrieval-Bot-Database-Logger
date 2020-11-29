bff1c8d242e56ef1245eeca44508031459b22720
negustaf <negustaf@umich.edu>
2020-11-29 15:14:20 -0500

    commit 1

---
+++ WebScraper.py
@@ -0,0 +1,23 @@
+
+# Noah Gustafson & Maxton Fil
+
+# commit 1
+
+from bs4 import BeautifulSoup
+import requests
+import re
+import os
+import csv
+
+def getKeyword(keyword):
+    soup = BeautifulSoup(requests.get('https://www.linkedin.com/jobs/search?').text, 'html.parser')
+    
+def getLocation(location):
+def getCompany(company):
+
+def main():
+
+if __name__ == '__main__':
+    main()
+    print(extra_credit("extra_credit.htm"))
+    unittest.main(verbosity=2)
