3a4a0f55beab978c9b5b82b2a3cae2971e2d746a
negustaf <negustaf@umich.edu>
2020-11-29 16:52:13 -0500

    commit 2

--- WebScraper.py
+++ WebScraper.py
@@ -1,7 +1,7 @@
 
 # Noah Gustafson & Maxton Fil
 
-# commit 1
+# commit 2
 
 from bs4 import BeautifulSoup
 import requests
@@ -9,15 +9,22 @@ import re
 import os
 import csv
 
-def getKeyword(keyword):
-    soup = BeautifulSoup(requests.get('https://www.linkedin.com/jobs/search?').text, 'html.parser')
+def getKeyword(keyword, locale):
+    #soup = BeautifulSoup(requests.get('https://www.linkedin.com/jobs/search?').text, 'html.parser')
+    soup = BeautifulSoup(requests.get(f'https://www.linkedin.com/jobs/search?keywords={keyword}&location={locale}-fl&redirect=false&position=1&pageNum=0').text, 'html.parser')
+    aTags = soup.find_all("a", class_="result-card__full-card-link")
+    print(aTags)
+    
     
 def getLocation(location):
+    pass
+
 def getCompany(company):
+    pass
 
 def main():
 
 if __name__ == '__main__':
     main()
-    print(extra_credit("extra_credit.htm"))
+    #print(extra_credit("extra_credit.htm"))
     unittest.main(verbosity=2)
