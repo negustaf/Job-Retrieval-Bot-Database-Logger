e2a611ea4fe5a4b5b42c38d10975634580008979
negustaf <negustaf@umich.edu>
2020-12-09 23:33:03 -0500

    updated web scraper

--- WebScraper.py
+++ WebScraper.py
@@ -7,7 +7,7 @@ import os
 # Class to be imported to Slack bot.
 class WebScraperBot:
 
-    # __init__() takes in slackChannel, keyword, and locale as parameters. Then the class constructor gets html from the param-specified LinkedIn jobs search page and sets soup as an instance.
+    # __init__() takes in slackChannel, keyword, and locale as parameters. Then the class constructor gets html code from the param-specified LinkedIn jobs search page and sets soup as an instance.
     def __init__(self, slackChannel, keyword, locale):
         self.slackChannel = slackChannel
         self.keyword = keyword#.replace(keyword, '-', 1)
@@ -15,7 +15,7 @@ class WebScraperBot:
         link = f'https://www.linkedin.com/jobs/search?keywords={keyword}&location={locale}-fl&redirect=false&position=1&pageNum=0'
         self.soup = BeautifulSoup(requests.get(link).text, 'html.parser')
 
-    # fetchTitles() finds all h3 links with specified class 'result-card__full-card-link' in each job card and assigns them to aTags. Then it finds all job titles in aTags and appends them to positionTitles.
+    # fetchTitles() finds all h3 links with specified class "result-card__full-card-link" in each job card and assigns them to aTags. Then it finds all job titles in aTags and appends them to the list posTitles. It returns posTitles.
     def fetchTitles(self):
         aTags = self.soup.find_all('a', class_='result-card__full-card-link')
         posTitles = []
@@ -24,15 +24,15 @@ class WebScraperBot:
             posTitles.append(title.text)
         return posTitles
     
-    # fetchCompanies() finds all h4 links with specified class 'job-result-card__subtitle-link' in each job card and assigns them to aTags2. Then it finds all companies in aTags2 and appends them to companyLst.
+    # fetchCompanies() finds all h4 links with specified class "job-result-card__subtitle-link" in each job card and assigns them to aTags. Then it finds all companies in aTags and appends them to the list posCompanies. It returns posCompanies.
     def fetchCompanies(self):
-        aTags2 = self.soup.find_all('a', class_='job-result-card__subtitle-link')
+        aTags = self.soup.find_all('a', class_='job-result-card__subtitle-link')
         posCompanies = []
-        for tag in aTags2:
+        for tag in aTags:
             posCompanies.append(tag.text)
         return posCompanies
 
-    # fetchURLs() again finds all h3 links with specified class 'result-card__full-card-link' in each job card and assigns them to aTags. Then it finds all URLs in aTags and appends them to urlList.
+    # fetchURLs() again finds all h3 links with specified class "result-card__full-card-link" in each job card and assigns them to aTags. Then it finds all URLs in aTags and appends them to the list posURLs. It returns posURLs.
     def fetchURLs(self):
         aTags = self.soup.find_all('a', class_='result-card__full-card-link')
         posURLs = []
@@ -41,14 +41,14 @@ class WebScraperBot:
             posURLs.append(urlHref)
         return posURLs
     
-    # combinedPositionTups() takes lists from posTitles, posCompanies, and posURLs and returns the items from each list in an order-respective list of tuples.
+    # combinedPositionTups() takes lists from posTitles, posCompanies, and posURLs and returns the items from each list in an order-respective list of tuples called posData. It returns posData. If the data has no new titles, it returns a message telling the user to try again.
     def combinedPosTups(self):
-        posDataLst = []
+        posData = []
         if self.fetchTitles() != None:
             for i in range(len(self.fetchTitles())):
                 tup = (self.fetchTitles()[i], self.fetchCompanies()[i], self.fetchURLs()[i])
-                posDataLst.append(tup)
-            return posDataLst
+                posData.append(tup)
+            return posData
         else:
             return 'There are no new job postings for this position. Try again in 10 seconds.'
     
@@ -66,27 +66,24 @@ class WebScraperBot:
                 comStr += ', '.join(i)
                 return comStr
     
-    # insertPositionsIntoSlackMessageDict() takes craftPosStr()'s returned tuple list of recent job position data and puts it into the Slack message dictionary.
+    # insertPositionsIntoSlackMessageDict() takes craftPosStr()'s returned tuple list of recent job position data and puts it into the Slack message dictionary. It returns the dictionary.
     def insertPosIntoSlackMessageDict(self):
         text = self.craftPosStr()
         return {"type": "section", "text": {"type": "mrkdwn", "text": text}},
 
-    # craftMessage() contains a constant, SLACK_MESSAGE, that contains the text displayed in the Slack message.
+    # craftMessage() contains a constant, SLACK_MESSAGE, that contains the text formatted to be displayed in the Slack message. It returns the Slack message-compatible dictionary with an output of the various instances.
     def craftMessage(self):
-        #keyword = self.keyword.replace(self.keyword, ' ', 1)
-        #locale = self.locale.replace(self.locale, ' ', 1)
         SLACK_MESSAGE = {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
                 "text": (
-                    #"testing string only"
                     f"The five most recent {self.keyword} positions in {self.locale} are: \n{self.craftPosStr()}"
-                ),# + {self.craftPosStr()} # \n\nClick here to see more data about the most recent {self.keyword} positions in {self.locale}
+                ),
             },
         }
         return SLACK_MESSAGE
-    # getMessagePayload() crafts and returns the entire message payload as a dictionary.
+    # getMessagePayload() crafts a message for the specified channel and returns the entire message payload as a dictionary.
     def getMessagePayload(self):
         return {
             "channel": self.slackChannel,
